42901d38c48af656f425fa5cc30f28aae2058be3
LeanickOjeda <101678421+LeanickOjeda@users.noreply.github.com>
2022-05-17 19:22:53 -0400

    mas ocsas

--- formulario.js
+++ formulario.js
@@ -10,6 +10,30 @@ $(document).ready(function(){
       else{
         $(location).attr("href","http://www.google.cl")
       }
-      
     });
+
+    $("#crear").click(function(){
+      var nombre = $("#nombre").val();
+      var apellido = $("#apellido").val();
+      var correo = $("#correo").val();
+      var clave = $("#clave").val();
+      if(nombre.length == 0){
+          alert("Introduzca su nombre")
+      }
+      if(apellido.length == 0){
+          alert("Introduzca su apellido")
+      }
+      if(correo.length == 0){
+          alert("Introduzca su correo")
+      }
+      
+      if(clave.length == 0){
+          alert("Introduzca una clave")
+      }
+    });
+      
+    
 })
+
+
+
